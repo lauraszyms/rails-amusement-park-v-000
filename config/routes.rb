06e89edeb5adf90f078632944dbd178db03ce9ef
lauraszyms <lauraszyms@gmail.com>
2018-01-23 00:37:00 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
+root to: 'application#home'
 
+resources :users
+resources :attractions
+resources :rides, only: [:create]
+resources :sessions, only: [:create, :destroy]
+get '/signin' => 'sessions#new'
 end
